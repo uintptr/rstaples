a79def97000de6d610c0048ca0cd2a31d3742088
fsck <inaforloop@gmail.com>
2025-09-30 19:49:31 -0400

    impl Error

--- src/error.rs
+++ src/error.rs
@@ -28,6 +28,8 @@ pub enum Error {
     LoggingError(log::SetLoggerError),
 }
 
+impl std::error::Error for Error {}
+
 impl core::fmt::Display for Error {
     fn fmt(&self, fmt: &mut core::fmt::Formatter) -> core::result::Result<(), core::fmt::Error> {
         write!(fmt, "{self:?}")
